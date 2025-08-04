d7356ff14f145389c91e5cc3bbba18381788c002
Jayant Aggarwal <jayantaggarwal@Jayants-MacBook-Air.local>
2025-08-04 16:25:42 +0530

    added redirect url

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ generation_status = {}  # Track generation progress
 # Google OAuth config
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-REDIRECT_URI = "http://localhost:8000/auth/callback"
+REDIRECT_URI = os.getenv("REDIRECT_URI", "http://localhost:8000/auth/callback")
 
 client_config = {
     "web": {
